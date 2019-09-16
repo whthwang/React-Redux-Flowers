a9c796a3addcbf88d7806c1cfdbc80290d3455f6
wayne hwang <whthwang@gmail.com>
2019-09-15 21:09:26 -0700

    [prog] componentDidMount function for sending data to the store

--- client/components/Users.jsx
+++ client/components/Users.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Users extends Component {
+
+  //when component renders, fetch all users from the db and send to redux store
+  componentDidMount() {
+    axios.get('/api/users')
+      .then((allUsers) => {
+        console.log('all users from db', allUsers);
+      })
+      .catch((error) => {
+        console.log(`axios get error: ${error}`);
+      })
+  }
 
   render() {
     return (
