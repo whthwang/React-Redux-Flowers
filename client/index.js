192b6b002b92e6caa125b602e94e46f361a2e11f
wayne hwang <whthwang@gmail.com>
2019-09-19 12:04:47 -0700

    added and adjusted comments

--- client/index.js
+++ client/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 import { createStore, applyMiddleware } from 'redux';
-import { userReducer } from './redux/reducers.js';
 import { Provider } from 'react-redux';
+import App from './components/App.jsx';
+import { userReducer } from './redux/reducers.js';
 
 //middlewares
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
-//import saga
+//saga
 import { watchFetchUsersAsync } from './redux/sagas.js';
 
 //middleware setup
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware, logger];
 
-//sagaMiddleware.run() has to run after the store is set up
+//redux store set up
 const store = createStore(userReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(watchFetchUsersAsync);
 
